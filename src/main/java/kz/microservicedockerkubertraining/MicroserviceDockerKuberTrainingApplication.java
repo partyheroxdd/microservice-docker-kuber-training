e311ac47b32f69c809068e760ff2159d66351cd4
Miras Madiyev <miras.madiev15@gmail.com>
2022-04-02 23:34:33 +0600

    Starting project

---
+++ src/main/java/kz/microservicedockerkubertraining/MicroserviceDockerKuberTrainingApplication.java
@@ -0,0 +1,13 @@
+package kz.microservicedockerkubertraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDockerKuberTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDockerKuberTrainingApplication.class, args);
+	}
+
+}
